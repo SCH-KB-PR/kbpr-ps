de88e72ac0c96ad9ca0ab1a22b381874778cbab9
Kovács Bálint <balintkovacs11@gmail.com>
2024-05-14 16:00:32 +0200

    aaaa

--- document.jsx
+++ document.jsx
@@ -3,11 +3,57 @@ const inchToMm = 25.4;
 const pixelToMm = inchToMm / ppi;
 
 
+
+
+
+function psInit() {
+    preferences.rulerUnits = Units.MM;
+    preferences.typeUnits = TypeUnits.MM;
+}
+
+function wasteCheck(docWidth, tileWidth, gutter) {
+    width = tileWidth;
+    while (width + tileWidth + gutter < docWidth) {
+        width += tileWidth + gutter;
+    }
+    return docWidth - width;
+}
+
+
 function newDocument() {
-    if (!dummyCheck()) return false;
-    
     $.writeln(selectedPaperSize);
     $.writeln(selectedPaperSize.name);
     $.writeln(selectedPaperSize.width);
     $.writeln(selectedPaperSize.height);
+    $.writeln(selectedRollWidth);
+    
+    if (!dummyCheck()) return false;
+
+    if (selectedPaperSize == PaperSizes.OTHER || selectedPaperSize == PaperSizes.POSTER || selectedPaperSize == PaperSizes.Sticker) {
+        alert("Nem implementált funkció!");
+        return false;        
+    }
+
+    psInit();
+
+    var paperSize = selectedPaperSize;
+    var documentWidth = selectedRollWidth - 10.4; // TODO: ask Gúz
+    var documentHeight = 0;
+    
+    var roate = false;
+
+    
+    if (paperSize.width < documentWidth) {
+        
+    }
+    else if (paperSize.height < documentWidth) {
+        
+    }
+    else {
+        alert("Nem fér el a kiválasztott méretű papíron!");
+        return false;
+    }
+
+
+
 }
