b142856c565625724f47218cf4d3ab0a9b46fd30
Kovács Bálint <balintkovacs11@gmail.com>
2024-10-28 16:29:44 +0100

    margin adjust

--- src/presets.jsx
+++ src/presets.jsx
@@ -39,6 +39,6 @@ var ppi = ResolutionArray[0];               // default resolution = 300 dpi
 
 // plotter dependent settings
 // dont touch unless you know what you are doing
-var margin = 3;                             // default margin = 3 mm
+var margin = 0;                             // default margin = 0 mm, the printer will add its 3mm by itself
 const maxDocumentHeight = 18000;            // 18 meters (as if photoshop could handle that lmao)
 const minDocumentHeight = 101.6;            // 101.6 mm = 4 inches
